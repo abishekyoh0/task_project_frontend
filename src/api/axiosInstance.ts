8d07c7cc2f1965558331dcc8e25e2bba7e611800
sivayoho <sivashankar.yoho@gmail.com>
2026-07-15 13:33:08 +0530

    Correction

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://eventflow-i1p9.onrender.com:3000/api/v1',
+  baseURL: 'https://eventflow-i1p9.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
